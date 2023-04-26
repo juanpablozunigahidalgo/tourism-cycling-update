4b2aead8474ed9feff95bcfd5f0fea22bdf82fa6
JuanPabloZ <juanpablo.zunigah@gmail.com>
2023-04-26 16:25:12 +0200

    structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Container from '@mui/material/Container';
-// import ImageSection from './components/imagesection/imagesection';
-
-import Footer from './components/footer/footer';
-import Header from './components/header/header';
-
-
-import ThemeProvider from '@mui/material/styles/ThemeProvider';
-import theme from './styles/main/typography';
-import Box from '@mui/material/Box';
-import Box1 from './components/matrix/box1';
-import Pagetitle from './components/pagetitle/pagetitle';
-
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/home';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Box
-        display="flex"
-        flexDirection="column"
-        minHeight="60vh"
-      >
-        <Header />
-        <Pagetitle></Pagetitle>
-        <Box flexGrow={1}>
-          <Container maxWidth="lg">
-            {/* <ImageSection /> */}
-            <Box1></Box1>
-          </Container>
-        </Box>
-        <Footer />
-      </Box>
-    </ThemeProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<Navigate to="/home" replace />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import '../App.css';
 import Container from '@mui/material/Container';
 // import ImageSection from './components/imagesection/imagesection';
 
